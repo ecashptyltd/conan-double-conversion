eb1557591201a6e54c8ece46f37c4fd007471da4
Uilian Ries <uilianries@gmail.com>
2018-10-12 14:31:38 -0300

    Update Conan convention
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 from bincrafters import build_template_default
 
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder(pure_c=False)
+    builder = build_template_default.get_builder(pure_c=True)
     builder.run()
